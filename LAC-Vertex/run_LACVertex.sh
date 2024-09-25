be9f7e67149e2b9eafaf000c828b823eba17c3b9
Zeynep Korkmaz <zkorkmaz@uwaterloo.ca>
2024-09-25 12:56:17 -0400

    Update run_LACVertex.sh

--- LAC-Vertex/run_LACVertex.sh
+++ LAC-Vertex/run_LACVertex.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 CMD="time ./LacVertex"
 DASH=_
 
 #Parameters:
-vertex_file="datasets/socLJ_goNode.txt"
-edge_file="datasets/socLJ_goEdge.txt"
+vertex_file="datasets/socLJ_goDBGNode.txt"
+edge_file="datasets/socLJ_goDBGEdge.txt"
+#vertex_file="datasets/socLJ_goNode.txt"
+#edge_file="datasets/socLJ_goEdge.txt"
 workload_file="workloads/WorkloadTraceSocLJ500K"
-
 workload_size=500000
 cache_size=20
-policy=1
+policy=3
 query_type=4
 SP_r=3
 NW=14
@@ -24,7 +26,7 @@ eval $CMDTEXT
     # do
 	#     for cache_size in 2 5 10 20 30
 	#     do
-	# 		for policy in 1 2
+	# 		for policy in 1 3
 	# 		do
 	# 		    FILENAME="$vertex_file$DASH$query_type$DASH$cache_size$DASH$policy.txt"
 	# 		    CMDTEXT="$CMD $vertex_file $edge_file $workload_file $workload_size $cache_size $policy $query_type $SP_r $NW $NS"
