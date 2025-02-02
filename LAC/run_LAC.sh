8eb3d9c04a3198ce41af2e90b5dc023a4c4583ba
Zeynep Korkmaz <zkorkmaz@uwaterloo.ca>
2025-02-01 19:09:39 -0500

    Update run_LAC.sh

--- LAC/run_LAC.sh
+++ LAC/run_LAC.sh
@@ -2,8 +2,7 @@
 CMD="./Lac"
 DASH=_
 
-#For Twitter graph, Please set workload size 500K for FR-ALL, FR-HOT and RW queries.
-#Even small cache percentages creates large number cache pages for Twitter and results in less than 0.01 fault rates.
+
 #Parameters:
 vertex_file="datasets/socLJ_goNode.txt"
 edge_file="datasets/socLJ_goEdge.txt"
